fa961ecd36351d836cd18916d977034335f79319
Sebastian Grobelny <sgrobelny@Sebastians-MacBook-Air-2.local>
2016-03-25 18:51:42 -0700

    Beginner Swift App
    Basic app that guesses how many fingers your phone is holding up. The
    phone is assigned a random number whose range is 5. Must be run on
    Xcode.

--- FingerGuesser/ViewController.swift
+++ FingerGuesser/ViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var AmountofCorrect: UITextField!
+    @IBOutlet weak var YourGuess: UITextField!
+    
+    @IBAction func Submit(sender: AnyObject) {
+        
+        let randinteger = UInt32(arc4random()%6);
+        AmountofCorrect.text = String(randinteger);
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
